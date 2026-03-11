1820ffbcb3a1470d8203b67f020d062a358ac59d
eimiii1 <philipjohn1627@gmail.com>
2026-03-11 23:06:00 +0800

    feat: add REST API with full CRUD for users resource

---
+++ 03-rest-apis/projects/basic-crud/server.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()) // middleware
+
+const users = [];
+
+app.get('/users', (req, res) => {
+    res.status(200);
+    res.json(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id; // get id request
+    const user_found = users.find(user => user.id === parseInt(id)) // find the user && if user not exist, return 'undefined'
+
+    if (!user_found) {
+        res.status(404);
+        res.json({ status: 404, message: "User not found." })
+        return;
+    }
+
+    res.status(200);
+    res.send(user_found)
+})
+
+app.post('/users', (req, res) => {
+    const new_user = { id: users.length + 1, ...req.body }
+    users.push(new_user)
+    res.status(201);
+    res.json({ status: 201, message: "User Added.", user: { id: new_user.id, name: new_user.name, age: new_user.age } })
+})
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user_found = users.find(user => user.id === parseInt(id))
+
+    if (!user_found) {
+        res.status(404);
+        res.json({ status: 404, message: "User not found." })
+        return;
+    }
+
+    Object.assign(user_found, req.body)
+    res.status(200)
+    res.json({ status: 200, message: "User updated." })
+})
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user_found = users.find(user => user.id === parseInt(id))
+
+    if (!user_found) {
+        res.status(404)
+        res.json({status: 404, message: "User not found."})
+        return;
+    }
+    
+    // find index of the found user
+    const user_index = users.findIndex(user => user.id === parseInt(id))
+    users.splice(user_index, 1);
+    res.status(200)
+    res.json({status: 200, message: "User Deleted."})
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
